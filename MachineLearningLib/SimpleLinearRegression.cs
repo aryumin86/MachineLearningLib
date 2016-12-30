74f48ce295126adda76c501d0d0798e12452c0b4
aryumin <aryumin@aryumin.ru>
2016-12-30 13:04:23 +0300

    edited namepace

--- MachineLearningLib/SimpleLinearRegression.cs
+++ MachineLearningLib/SimpleLinearRegression.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MachineLearningLib
+namespace ru.aryumin.MachineLearningLib
 {
     /// <summary>
     /// Simple linear regression
